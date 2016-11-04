7711bb943570b2957e51bdae50c2828fcd014df4
Darshit Shah <darshit@yuktianalytics.com>
2016-11-04 15:17:35 +0530

    backup portion is almost done.

--- index.js
+++ index.js
@@ -1,164 +1,134 @@
 #!/usr/bin/env node
-var debug = require('debug')('infinidb-management-tool-master:infinidb-management-tool-master');
+
+var debug = require('debug')('infinidb-management-tool:infinidb-management-tool');
 var fs = require('fs');
 var queryExecutor = require('node-database-executor');
-var defaultDBConfig = {
-	type: "database",
-	databaseType: "mysql",
-	engine: "infinidb",
-	database: "",
-	host: "",
-	port: "",
-	user: "",
-	password: "",
-	connectionLimit: 0
-};
 var argv = require('yargs').argv;
-console.log(argv.host);
-console.log(argv.db);
-console.log(argv.tmpdir);
+debug(argv);
 
-if (argv.host && argv.db && argv.user && argv.pass && argv.tmpdir && argv.expdir && argv.type) {
-    console.log('Good to go!');
-} 
-else {
-    console.log('You shall not pass!');
+if (argv.action && argv.engine && argv.port && argv.host && argv.db && argv.user && argv.pass && argv.tmpdir) {
+  console.log('Good to go!');
+} else {
+  console.log('You shall not pass!',argv.action, argv.engine, argv.port, argv.host, argv.db, argv.user, argv.pass, argv.tmpdir);
+  process.exit(0);
 }
 
-var dirTempo = argv.tmpdir;
+var defaultDBConfig = {
+  type: "database",
+  databaseType: "mysql",
+  engine: argv.engine,
+  database: argv.db,
+  host: argv.host,
+  port: argv.port,
+  user: argv.user,
+  password: argv.pass,
+  connectionLimit: 0
+};
 
-var createStreamSQL = fs.createWriteStream("F:/filemanagement/tempo/DBBackupScript.sql", {'flags': 'a'});
-var createStreamTXT = fs.createWriteStream("F:/filemanagement/tempo/TableMapping.txt");
-var varShowCreateTable = "";
-if (!fs.existsSync(dirTempo)){
-	console.log('Directory already exists! Please delete it manually.');
-	process.exit(0);
+
+var backupDirectory = argv.tmpdir;
+// backupDirectory = "F:/filemanagement/tempo";
+
+if (argv.action.toString() == "restore") {
+	dropRecreateTables(defaultDBConfig.database, function(){
+		process.exit(0);
+	});
 }
-else 
-	// create directory
-	// fs.mkdirSync(dir);
-	var dir = 'F:/filemanagement/tempo/';
-	console.log(dir);
-	if (!fs.existsSync(dir)) {
-		console.log('Folder does not exists!');
-		return;			
-	} else {
-       	// create directory
-		// fs.mkdirSync(exports.folderPath + 'F:/filemanagement/tempo/');
-				
-		//createStreamSQL.write("use axiomdemo;\n");
-		//"use argv.db;"
-		createStreamSQL.write("use " + argv.db + ";\n"); 
-		
-		createStreamSQL.write("show tables;\n");
-		//"show tables;"
-		getTables(argv.db, function(resultTables){
-			debug("After getting table(s) " + JSON.stringify(resultTables));
-			//process.exit(0);
-		});
-		
-		createStreamTXT.write("tablename           |           filename\r\n");
-		createStreamTXT.write("temp");
-		
-		// createStream.write("show create table temp;\n");
-		// createStreamSQL.write("drop table if exists temp;\n");
-		getShowCreateTable('temp', function(resultCreateTable){ 
-			debug("After getting show create table " + JSON.stringify(resultCreateTable));
-			debug(resultCreateTable.content[0]['Table']);
-			debug(JSON.stringify(resultCreateTable.content[0]['Create Table']));
-			varShowCreateTable += JSON.stringify(resultCreateTable.content[0]['Create Table']) + ";\n";
-			console.log(varShowCreateTable);
+else if(argv.action.toString() == "backup") {
+  var createStreamSQL = fs.createWriteStream(backupDirectory + "/DBBackupScript.sql", { 'flags': 'w' });
+  var createStreamTXT = fs.createWriteStream(backupDirectory + "/TableMapping.txt");
+  var varShowCreateTable = "";
+  if (fs.existsSync(backupDirectory)) {
+    console.log('Directory already exists! Please delete it manually.');
+    // process.exit(0);
+  }
+  // create directory
+  // fs.mkdirSync(backupDirectory);
 
-			createStreamSQL.write("drop table if exists temp;\n");
-			
-			createStreamSQL.write(resultCreateTable.content[0]['Create Table'] + ";\n");
-			
-		});
-
-		getOutFile(argv.db, function(resultOutFile){
-			createStreamSQL.write(resultOutFile + ";\n");
-			
-		});
-		
-		//toEnd();
-		/*createStreamSQL.end();
-		createStreamTXT.end();*/
-		
-		console.log('Inside');
+  createStreamTXT.write("tableName,fileName\r\n");
+  getTables(function(resultTables) {
+    if (resultTables.status === false) {
+      console.log(resultTables);
+      return;
     }
 
-    function getShowCreateTable(temp,cbFunc) { 
-		// "show create table temp", 
-		var query = "show create table " + temp; 
-		var requestData = {};
-		requestData.query = query;
-		requestData.dbConfig = defaultDBConfig;
-		//debug(requestData.dbConfig);
-		queryExecutor.executeRawQuery(requestData, function(data) {
-            if (data.status == true) {
-		        cbFunc({
-		        	status: true,
-		            content: data.content
-		        });
-		    } else {
-		        cbFunc({
-		            status: false,
-		            error: data.error
-		        });
-		    }
+    function processTables(index) {
+      if (index >= resultTables.content.length) {
+        createStreamSQL.end();
+        createStreamSQL.destroySoon();
+        createStreamTXT.end();
+        createStreamTXT.destroySoon();
+        process.exit(0);
+        return;
+      }
+      var currTableName = resultTables.content[index]["Tables_in_" + defaultDBConfig.database];
+      debug((index + 1) + " out of " + resultTables.content.length + ". Processing Table " + currTableName);
+      getShowCreateTable(currTableName, function(tableData) {
+        if (tableData.status === false) {
+          console.log(tableData);
+          return;
+        }
+        createStreamSQL.write("drop table if exists `"+currTableName+"`;\r\n"+ tableData.content[0]['Create Table'] + ";\r\n\r\n");
+        getOutFile(currTableName, function(queryResult) {
+          if (queryResult.status === false) {
+            console.log(queryResult);
+            return;
+          }
+          processTables(index + 1);
         });
-	}
-	function getTables(db,cbFunc){
-		var query = "show tables"; 	
-		var requestData = {};
-		requestData.query = query;
-		debug(requestData.query);
-		requestData.dbConfig = defaultDBConfig;	
-		queryExecutor.executeRawQuery(requestData, function(data) {
-            if (data.status == true) {
-		        cbFunc({
-		        	status: true,
-		            content: data.content
-		        });
-		    } else {
-		        cbFunc({
-		            status: false,
-		            error: data.error
-		        });
-		    }
-        });	
-	}
-	function getOutFile(db,cbFunc){
-		var query = "select * from temp" + " into OUTFILE 'F:/filemanagement/BackupTempo/Datatempo.csv'" + " fields terminated by ',' enclosed by '\"' lines terminated by '\r\n' ";
-		debug(query);
+      });
+    }
+    processTables(0);
+  });
+}
+else {
+	console.log("Unknown action "+argv.action);
+	process.exit(0);
+}
+
+function getTables(cb) {
+  var query = "show tables;";
+  var requestData = {};
+  requestData.query = query;
+  debug(requestData.query);
+  requestData.dbConfig = defaultDBConfig;
+  queryExecutor.executeRawQuery(requestData, cb);
+}
+
+function getShowCreateTable(tableName, cb) {
+  var query = "show create table `" + tableName + '`;';
+  var requestData = {};
+  requestData.query = query;
+  requestData.dbConfig = defaultDBConfig;
+  queryExecutor.executeRawQuery(requestData, cb);
+}
+
+function getOutFile(tableName, cb) {
+  createStreamTXT.write(tableName + "," + tableName + "\r\n");
+  var query = "select * from `" + tableName + "` into OUTFILE '" + backupDirectory + "/" + tableName + ".csv'" + " fields terminated by ',' enclosed by '\"' lines terminated by '\r\n';";
+  debug(query);
+  var requestData = {
+    "query": query,
+    "dbConfig": defaultDBConfig
+  };
+  queryExecutor.executeRawQuery(requestData, cb);
+}
+
+function dropRecreateTables(dbName, cb){
+	fs.readFile(backupDirectory+ "/DBBackupScript.sql", function(err, data){
 		var requestData = {
-            "query": query,
-            "dbConfig": defaultDBConfig
-        };           
-		queryExecutor.executeRawQuery(requestData, function(data) {
-			debug("After getting OUTFILE " +  JSON.stringify(data));
-		    if (data.status == true) {
-		        cbFunc({
-		        	status: true,
-		        	content: data.content
-		        });
-		    } else {
-		        cbFunc({
-		            status: false,
-		            error: data.error
-		        });
-		    }
-        });	
+	    "query": "use "+dbName+";" + data,
+	    "dbConfig": defaultDBConfig
+	  };
+	  queryExecutor.executeRawQuery(requestData, cb);
+	});
+}
 
-	}	
-	function toEnd(){
-		createStreamSQL.end();
-		createStreamTXT.end();	
-	}
+// tablename | filename
+// loop
+// 	show create table
+// 		drop table if exists && create table script >> create_table.sql
 
-	// tablename | filename
-	// loop
-	// 	show create table
-	// 		drop table if exists && create table script >> create_table.sql
-
-// ./index.js --host="192.168.1.1" --db="192.168.1.1" --user="192.168.1.1" --pass="192.168.1.1" --tmpdir="192.168.1.1" --expdir="192.168.1.1" --type="192.168.1.1" 
+/*
+node index.js --action="backup" --engine="infinidb" --port="3307" --host="52.220.65.105" --db="axiomdemo" --user="usr" --pass="usr" --tmpdir="/tmp/database_backup" 
+*/
